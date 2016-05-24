8360ec1774746298944a4626e36784355a92c54a
Eriks Ocakovskis <e.ocakovskis@gmail.com>
2016-05-24 10:00:15 +0300

    Change wait time to 4:58

--- dont_lock_me_out.py
+++ dont_lock_me_out.py
@@ -1,5 +1,5 @@
 # Don't log me out
-# Version 0.0.1
+# Version 0.0.2
 
 import sys
 import time
@@ -16,14 +16,14 @@ def main_loop():
 
     win32api.SetCursorPos((int(cx),int(cy)))
 
-    next_call = next_call + 600;
+    next_call = next_call + 298;
     if (next_call - time.time()) <= 0:
       time.sleep(10)
     else:
       time.sleep(next_call - time.time())
 
 if __name__ == "__main__":
-  print "You will not be logged out."
+  print "You will not be locked out."
   print "Close this window when you are done."
   timerThread = Thread(target=main_loop)
   timerThread.start()
